1d9098d6e029e98467c5fc7212d9c135ff8b49ee
Your Name <promiseejiro43@gmail.com>
2024-07-13 23:17:59 +0100

    new commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    source: "api/products",
+    destination: "https://api.timbu.cloud/products",
+    images: {
+      domains: ["api.timbu.cloud", "encrypted-tbn0.gstatic.com"],
+    },
+  };
+  
+  export default nextConfig;
+  
